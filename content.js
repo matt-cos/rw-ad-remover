ecf4cd11c36d773ca409e10335c00a556ed33d8b
Matt Cosentino <matt@learnedmedia.com>
2018-04-04 21:08:45 -0400

    First Commit

---
+++ content.js
@@ -0,0 +1,3 @@
+$('head').append('<style>#block-zeus-newsletter-ad-block-signup{display:none !important;}.ad-blocker-custom-blur{filter:none !important;opacity:1 !important}</style>');
+
+// https://stackoverflow.com/questions/16921685/change-the-chrome-extension-icon
