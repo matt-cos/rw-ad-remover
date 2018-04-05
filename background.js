b689576e3b760e4d1a518ea9432662a1da9a9eb2
Matt Cosentino <matt@learnedmedia.com>
2018-04-04 22:18:38 -0400

    extension is functioning. trying to get icon to change color when it's active

---
+++ background.js
@@ -0,0 +1,28 @@
+// // Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// // Use of this source code is governed by a BSD-style license that can be
+// // found in the LICENSE file.
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+	
+// 	// Send a message to the active tab
+// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 		var activeTab = tabs[0];
+// 		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+// 	});
+
+// 	chrome.browserAction.setIcon({
+// 		path : "img/rw-active.png"
+// 	});
+// });
+
+
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		// read `newIconPath` from request and read `tab.id` from sender
+		chrome.browserAction.setIcon({
+			path: request.newIconPath
+			// tabId: sender.tab.id
+		});
+	}
+);
--- content.js
+++ content.js
@@ -1,3 +1,25 @@
-$('head').append('<style>#block-zeus-newsletter-ad-block-signup{display:none !important;}.ad-blocker-custom-blur{filter:none !important;opacity:1 !important}</style>');
+// chrome.runtime.onMessage.addListener(
+// 	function(request, sender, sendResponse) {
+// 		if( request.message === "clicked_browser_action" ) {
+// 			$('head').append('<style>#block-zeus-newsletter-ad-block-signup{display:none !important;}.ad-blocker-custom-blur{filter:none !important;opacity:1 !important}</style>');
+// 		}
+// 	}
+// );
+
+
+changeToActive = true;
+changeToInactive = false;
+
 
 // https://stackoverflow.com/questions/16921685/change-the-chrome-extension-icon
+if(changeToActive) {
+	// send message to background script
+	chrome.runtime.sendMessage({ "newIconPath" : "img/rw-active.png" });
+	$('head').append('<style>#block-zeus-newsletter-ad-block-signup{display:none !important;}.ad-blocker-custom-blur{filter:none !important;opacity:1 !important; pointer-events:auto !important;}</style>');
+}
+
+if(changeToInactive) {
+	// send message to background script
+	chrome.runtime.sendMessage({ "newIconPath" : "img/rw-inactive.png" });
+	$('head').append('<style>#block-zeus-newsletter-ad-block-signup{display:none !important;}.ad-blocker-custom-blur{filter:none !important;opacity:1 !important; pointer-events:auto !important;}</style>');
+}
